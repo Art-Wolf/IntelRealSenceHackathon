28cb6603cfeeba606694bac537d5f2aa21b68b96
John Doyle <john.c.doyle@gmail.com>
2015-09-30 07:47:38 -0700

    Moving Hand Cube
    Use keys to position the hand and swipe the track pad to move in the Z
    axis

---
+++ Assets/Script/MoveCamera.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour {
+
+	public float speed = 1.5f;
+
+	private float lastMousePosition = 0;
+
+	void Start () {
+		lastMousePosition = Input.GetAxis ("Mouse Y");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Translate (new Vector3 (speed * Time.deltaTime, 0, 0));
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate (new Vector3 (-speed * Time.deltaTime, 0, 0));
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.Translate (new Vector3 (0, -speed * Time.deltaTime, 0));
+		}
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.Translate (new Vector3 (0, speed * Time.deltaTime, 0));
+		}
+
+		float newMousePosition = Input.GetAxis ("Mouse Y");
+		if (newMousePosition != lastMousePosition) {
+			transform.Translate(new Vector3(0.0f, 0.0f, newMousePosition));
+		} 
+
+	}
+}
